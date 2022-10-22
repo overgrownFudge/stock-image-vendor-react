e4da236bfdecece75d9742989e5af03ab79bb206
overgrownFudge <overgrownfudge@gmail.com>
2022-10-22 17:07:00 -0400

    fixed format

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,23 +1,23 @@
-import React, {useContext} from "react"
+import React, { useContext } from "react"
 import PropTypes from "prop-types"
-import {Context} from "../Context"
+import { Context } from "../Context"
 import useHover from "../hooks/useHover"
 
-export default function CartItem({item}) {
+export default function CartItem({ item }) {
     const [hovered, ref] = useHover()
-    const {removeFromCart} = useContext(Context)
-    
+    const { removeFromCart } = useContext(Context)
+
     const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
-    
+
     return (
         <div className="cart-item">
-            <i 
-                className={iconClassName} 
+            <i
+                className={iconClassName}
                 onClick={() => removeFromCart(item.id)}
                 ref={ref}
             >
             </i>
-            
+
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
