cbbcdb7f11670b188b5d6b6cee41b8653c0b2f53
overgrownFudge <overgrownfudge@gmail.com>
2022-10-22 17:06:55 -0400

    fixed format

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react"
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 
-export default function App() {    
+export default function App() {
     return (
         <div>
             <Header />
             <Routes>
-                <Route path="/" element = {<Photos />} />
-                
-                <Route path="/cart" element = {<Cart />} />
+                <Route path="/" element={<Photos />} />
+
+                <Route path="/cart" element={<Cart />} />
             </Routes>
         </div>
     )
