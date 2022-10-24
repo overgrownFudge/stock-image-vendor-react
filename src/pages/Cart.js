b0efad166a51e4ad7eb32dc8eeb5ac0e1b83ac52
overgrownFudge <overgrownfudge@gmail.com>
2022-10-23 22:41:23 -0400

    fixed formatting

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,17 +1,17 @@
-import React, {useState, useContext} from "react"
-import {Context} from "../Context"
+import React, { useState, useContext } from "react"
+import { Context } from "../Context"
 import CartItem from "../components/CartItem"
 
 export default function Cart() {
     const [buttonText, setButtonText] = useState("Place Order")
-    const {cartItems, emptyCart} = useContext(Context)
+    const { cartItems, emptyCart } = useContext(Context)
     const totalCost = 5.99 * cartItems.length
-    const totalCostDisplay = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
-    
+    const totalCostDisplay = totalCost.toLocaleString("en-US", { style: "currency", currency: "USD" })
+
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item} />
     ))
-    
+
     function placeOrder() {
         setButtonText("Ordering...")
         setTimeout(() => {
@@ -20,7 +20,7 @@ export default function Cart() {
             emptyCart()
         }, 3000)
     }
-    
+
     return (
         <main className="cart-page">
             <h1>Check out</h1>
@@ -28,10 +28,10 @@ export default function Cart() {
             <p className="total-cost">Total: {totalCostDisplay}</p>
             {
                 cartItems.length > 0 ?
-                <div className="order-button">
-                    <button onClick={placeOrder}>{buttonText}</button>
-                </div> :
-                <p>You have no items in your cart.</p>
+                    <div className="order-button">
+                        <button onClick={placeOrder}>{buttonText}</button>
+                    </div> :
+                    <p>You have no items in your cart.</p>
             }
         </main>
     )
