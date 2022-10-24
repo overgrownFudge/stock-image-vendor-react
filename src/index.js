de7abea9386b2304f0f845849097655f0233a63b
overgrownFudge <overgrownfudge@gmail.com>
2022-10-23 22:41:20 -0400

    fixed formatting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
-import {ContextProvider} from "./Context"
+import { ContextProvider } from "./Context"
 import App from "./App"
 import "./styles.css"
 
