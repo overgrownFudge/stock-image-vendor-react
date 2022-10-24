83210f0e41f54fbaa83a2b5859d3f62776935cdf
overgrownFudge <overgrownfudge@gmail.com>
2022-10-23 22:41:28 -0400

    fixed formatting

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,16 +1,16 @@
-import React, {useContext} from "react"
+import React, { useContext } from "react"
 
 import Image from "../components/Image"
-import {Context} from "../Context"
-import {getClass} from "../utils"
+import { Context } from "../Context"
+import { getClass } from "../utils"
 
 export default function Photos() {
-    const {allPhotos} = useContext(Context)
-    
+    const { allPhotos } = useContext(Context)
+
     const imageElements = allPhotos.map((img, i) => (
         <Image key={img.id} img={img} className={getClass(i)} />
     ))
-    
+
     return (
         <main className="photos">
             {imageElements}
